213bbc04310f94f6257efcdcaf3ba05822162109
Sridhar S <srisridhar2002@gmail.com>
2024-02-01 09:42:34 +0530

    output included

--- src/com/training/day3/Employee.java
+++ src/com/training/day3/Employee.java
@@ -19,24 +19,3 @@ public class Employee implements Taxable{
     }
 }
 
-/*
-Output:
-Enter Employee Information:
-Employee ID: 12
-Employee Name: Ram
-Salary: $20000
-
-Calculating Income Tax:
-Income Tax for employee Ram with empId 12: $2100.0
-
-Enter Product Information:
-Product ID: 10
-Price per unit: $200
-Quantity: 400
-
-Calculating Sales Tax:
-Sales Tax for product with pid 10: $5600.000000000001
-
-Process finished with exit code 0
-
- */
